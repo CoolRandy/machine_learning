eda1e3ae5700a92f0a0749481f90d64cde04f3c1
randy <randy.lh@alibaba-inc.com>
2018-02-10 15:25:36 +0800

    完善线性回归预测

--- linear-regression/pltData.py
+++ linear-regression/pltData.py
@@ -18,6 +18,7 @@ def loadDataSet(fileName):
         """
     
     numFeat = len(open(fileName).readline().split('\t')) - 1
+    print 'numFeat: ', numFeat
     xArr = []; yArr = []
     fr = open(fileName)
     for line in fr.readlines():
@@ -27,7 +28,51 @@ def loadDataSet(fileName):
             lineArr.append(float(curLine[i]))
         xArr.append(lineArr)
         yArr.append(float(curLine[-1]))
+#    print xArr
+#    print yArr
     return xArr, yArr
+
+def standRegression(xArr, yArr):
+    """
+    Description:
+        根据推导出来的正规方程组来计算回归系数
+    Parameters:
+        xArr - x数据集
+        yArr - y数据集
+    Returns:
+        ws - 回归系数
+    """
+#   x矩阵
+    xMat = np.mat(xArr);
+#   y矩阵转置
+    yMat = np.mat(yArr).T;
+    xTx = xMat.T * xMat;
+#   判断矩阵是否可求逆
+    if np.linalg.det(xTx) == 0.0:
+        print ("矩阵为奇异矩阵，不可求逆")
+        return
+    ws = xTx.I * (xMat.T * yMat);
+    return ws;
+
+
+def plotRegression():
+    """
+    """
+    xArr, yArr = loadDataSet('ex0.txt');  #加载数据集
+    ws = standRegression(xArr, yArr);   #计算回归系数
+    xMat = np.mat(xArr)   #创建xMat矩阵
+    yMat = np.mat(yArr)    #创建yMat矩阵
+    xCopy = xMat.copy()      #深拷贝xMat矩阵
+    xCopy.sort(0)       #排序
+    yHat = xCopy * ws   #计算对应的y值
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  #添加subplot
+    ax.plot(xCopy[:, 1], yHat, c = 'red')                                #绘制回归曲线
+    ax.scatter(xMat[:,1].flatten().A[0], yMat.flatten().A[0], s = 20, c = 'blue',alpha = .5)                #绘制样本点
+    plt.title('DataSet')                                                #绘制title
+    plt.xlabel('X')
+    plt.show()
+
 
 def plotDataSet():
     """
@@ -42,15 +87,33 @@ def plotDataSet():
         """
     xArr, yArr = loadDataSet('ex0.txt')                                    #加载数据集
     n = len(xArr)                                                        #数据个数
+    print 'n: ', n
     xcord = []; ycord = []                                                #样本点
     for i in range(n):
         xcord.append(xArr[i][1]); ycord.append(yArr[i])                    #样本点
     fig = plt.figure()
     ax = fig.add_subplot(111)                                            #添加subplot
-    ax.scatter(xcord, ycord, s = 20, c = 'blue',alpha = .5)                #绘制样本点
+    ax.scatter(xcord, ycord, s = 20, c = 'green',alpha = .5)                #绘制样本点
     plt.title('DataSet')                                                #绘制title
     plt.xlabel('X')
+    plt.ylabel('Y')
     plt.show()
 
 if __name__ == '__main__':
-    plotDataSet()
+    #   python中提供了相关系数的计算方法
+    xArr, yArr = loadDataSet('ex0.txt')                                    #加载数据集
+    ws = standRegression(xArr, yArr)                                        #计算回归系数
+    xMat = np.mat(xArr)                                                    #创建xMat矩阵
+    yMat = np.mat(yArr)                                                    #创建yMat矩阵
+    yHat = xMat * ws
+    print (np.corrcoef(yHat.T, yMat))
+#    plotDataSet()
+    plotRegression();
+
+
+
+
+
+
+
+
